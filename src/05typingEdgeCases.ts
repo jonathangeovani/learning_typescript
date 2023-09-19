dcb4394de1cd635d7c3d6673fabcbe3a1f51fb61
jonathangeovani <dongeovanimenor@gmail.com>
2023-09-19 09:19:04 -0300

    create typing edge cases file

---
+++ src/05typingEdgeCases.ts
@@ -0,0 +1,2 @@
+const s1 = new Set<number>();       // We use "<type>" to say that the type of this "Set()" is number
+const s2: Set<number> = new Set();  // Or we can use : Set<number> to assing the type as well
